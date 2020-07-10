9a3ecfd09cb30ae338d877cd264aba502df5bc75
WyattBlue <57511737+WyattBlue@users.noreply.github.com>
2020-07-09 20:08:48 -0400

    20w28a

--- auto-editor.py
+++ auto-editor.py
@@ -3,27 +3,23 @@
 
 # Internal python libraries
 import os
-import argparse
 import re
-import subprocess
 import sys
 import time
 import platform
-from datetime import timedelta
+import argparse
+import subprocess
 from shutil import rmtree
+from datetime import timedelta
 from operator import itemgetter
 
-version = '20w27b hotfix'
+version = '20w28a'
 
-# files that start with . are hidden, but can be viewed by running "ls -f" from console.
+# Files that start with . are hidden, but can be viewed by running "ls -f" from console.
 TEMP = '.TEMP'
 CACHE = '.CACHE'
 
 def getFrameRate(path):
-    """
-    get the frame rate by asking ffmpeg to do it for us then using a regex command to
-    retrieve it.
-    """
     process = subprocess.Popen(['ffmpeg', '-i', path],
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, __ = process.communicate()
@@ -31,76 +27,94 @@ def getFrameRate(path):
     matchDict = re.search(r'\s(?P<fps>[\d\.]+?)\stbr', output).groupdict()
     return float(matchDict['fps'])
 
-
 def file_type(file):
     if(not os.path.isfile(file)):
         print('Could not locate file:', file)
-        sys.exit()
+        sys.exit(1)
     return file
 
+def float_type(num):
+    if(num.endswith('%')):
+        num = float(num[:-1]) / 100
+    else:
+        num = float(num)
+    return num
 
-def time_units(uinput):
-    try:
-        return int(uinput)
-    except ValueError:
-        if(uinput.endswith('secs')):
-            return int(uinput[:3])
-        else:
-            print('Incorrect format for time units')
-            sys.exit()
+def sample_rate_type(num):
+    if(num.endswith(' Hz')):
+        num = int(num[:-3])
+    elif(num.endswith(' kHz')):
+        num = int(float(num[:-4]) * 1000)
+    else:
+        num = int(num)
+    return num
 
 if(__name__ == '__main__'):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', nargs='*',
-        help='the path to the video file you want modified. can be a URL with youtube-dl.')
-    parser.add_argument('--output_file', '-o', type=str, default='',
-        help='name the output file.')
-    parser.add_argument('--silent_threshold', '-t', type=float, default=0.04,
-        help='the volume that frames audio needs to surpass to be sounded. It ranges from 0 to 1.')
-    parser.add_argument('--zoom_threshold', '-l', type=float, default=2.00,
-        help='the volume that needs to be surpassed to zoom in the video. (0-1)')
-    parser.add_argument('--video_speed', '--sounded_speed', '-v', type=float, default=1.00,
-        help='the speed that sounded (spoken) frames should be played at.')
-    parser.add_argument('--silent_speed', '-s', type=float, default=99999,
-        help='the speed that silent frames should be played at.')
-    parser.add_argument('--frame_margin', '-m', type=int, default=4,
-        help='tells how many frames on either side of speech should be included.')
-    parser.add_argument('--sample_rate', '-r', type=float, default=48000,
-        help='sample rate of the input and output videos.')
-    parser.add_argument('--audio_bitrate', type=str, default='160k',
-        help='number of bits per second for audio. Example, 160k.')
-    parser.add_argument('--frame_rate', '-f', type=float,
-        help='manually set the frame rate (fps) of the input video.')
-    parser.add_argument('--verbose', action='store_true',
-        help='display more information when running.')
-    parser.add_argument('--clear_cache', action='store_true',
-        help='delete the cache folder and all of its contents.')
-    parser.add_argument('--version', action='store_true',
-        help='show which auto-editor you have.')
-    parser.add_argument('--debug', action='store_true',
-        help='show helpful debugging values.')
-    parser.add_argument('--background_music', type=file_type,
-        help='add background music to your output.')
-    parser.add_argument('--background_volume', type=float, default=-8,
-        help="set the dBs louder or softer compared to the audio track that bases the cuts.")
-    parser.add_argument('--cut_by_this_audio', type=file_type,
-        help="base cuts by this audio file instead of the video's audio.")
-    parser.add_argument('--cut_by_this_track', '-ct', type=int, default=0,
-        help='base cuts by a different audio track in the video.')
-    parser.add_argument('--cut_by_all_tracks', action='store_true',
-        help='combine all audio tracks into 1 before basing cuts.')
-    parser.add_argument('--keep_tracks_seperate', action='store_true',
-        help="Don't combine audio tracks. (Warning, multiple audio tracks are not supported on most platforms)")
-    parser.add_argument('--hardware_accel', type=str,
-        help='set the hardware used for gpu acceleration.')
-    parser.add_argument('--combine_files', action='store_true',
-        help='combine all files in a folder before editing.')
-    parser.add_argument('--no_open', action='store_true',
+    parser = argparse.ArgumentParser(prog='Auto-Editor', usage='Auto-Editor: [options]')
+
+    basic = parser.add_argument_group('Basic Options')
+    basic.add_argument('input', nargs='*',
+        help='the path to the file, folder, or url you want edited.')
+    basic.add_argument('--frame_margin', '-m', type=int, default=4, metavar='',
+        help='set how many "silent" frames of on either side of "loud" sections be included.')
+    basic.add_argument('--silent_threshold', '-t', type=float_type, default=0.04, metavar='',
+        help='set the volume that frames audio needs to surpass to be sounded. (0-1)')
+    basic.add_argument('--video_speed', '--sounded_speed', '-v', type=float_type, default=1.00, metavar='',
+        help='set the speed that "loud" sections should be played at.')
+    basic.add_argument('--silent_speed', '-s', type=float_type, default=99999, metavar='',
+        help='set the speed that "silent" sections should be played at.')
+    basic.add_argument('--output_file', '-o', type=str, default='', metavar='',
+        help='set the name of the new output.')
+
+    advance = parser.add_argument_group('Advanced Options')
+    advance.add_argument('--no_open', action='store_true',
         help='do not open the file after editing is done.')
-    parser.add_argument('--preview', action='store_true',
+    advance.add_argument('--zoom_threshold', type=float_type, default=2.00, metavar='',
+        help='set the volume that needs to be surpassed to zoom in the video. (0-1)')
+    advance.add_argument('--combine_files', action='store_true',
+        help='when using a folder as the input, combine all files in a folder before editing.')
+    advance.add_argument('--hardware_accel', type=str, metavar='',
+        help='set the hardware used for gpu acceleration.')
+
+    audio = parser.add_argument_group('Audio Options')
+    audio.add_argument('--sample_rate', '-r', type=sample_rate_type, default=48000, metavar='',
+        help='set the sample rate of the input and output videos.')
+    audio.add_argument('--audio_bitrate', type=str, default='160k', metavar='',
+        help='set the number of bits per second for audio.')
+    audio.add_argument('--background_music', type=file_type, metavar='',
+        help='set an audio file to be added as background music to your output.')
+    audio.add_argument('--background_volume', type=float, default=-8, metavar='',
+        help="set the dBs louder or softer compared to the audio track that bases the cuts.")
+
+    cutting = parser.add_argument_group('Options for Cutting')
+    cutting.add_argument('--cut_by_this_audio', type=file_type, metavar='',
+        help="base cuts by this audio file instead of the video's audio.")
+    cutting.add_argument('--cut_by_this_track', '-ct', type=int, default=0, metavar='',
+        help='base cuts by a different audio track in the video.')
+    cutting.add_argument('--cut_by_all_tracks', action='store_true',
+        help='combine all audio tracks into one before basing cuts.')
+    cutting.add_argument('--keep_tracks_seperate', action='store_true',
+        help="don't combine audio tracks. mutually exclusive with cut_by_all_tracks.")
+
+    debug = parser.add_argument_group('Options for Debugging')
+    debug.add_argument('--verbose', action='store_true',
+        help='display more information when running.')
+    debug.add_argument('--clear_cache', action='store_true',
+        help='delete the cache folder and all its contents.')
+    debug.add_argument('--version', action='store_true',
+        help='show which auto-editor you have.')
+    debug.add_argument('--debug', action='store_true',
+        help='show helpful debugging values.')
+
+    misc = parser.add_argument_group('Options That Completely Change What Auto-Editor Does')
+    misc.add_argument('--preview', action='store_true',
         help='show stats on how the video will be cut.')
-    parser.add_argument('--export_to_premiere', action='store_true',
+    misc.add_argument('--export_to_premiere', action='store_true',
         help='export as an XML file for Adobe Premiere Pro instead of outputting a video.')
+
+    dep = parser.add_argument_group('Deprecated Options')
+    dep.add_argument('--frame_rate', '-f', type=float, metavar='',
+        help='manually set the frame rate (fps) of the input video.')
 
     args = parser.parse_args()
 
@@ -128,25 +142,15 @@ if(__name__ == '__main__'):
 
     if(args.input == []):
         print('auto-editor.py: error: the following arguments are required: input')
-        sys.exit()
+        sys.exit(1)
 
     INPUT_FILE = args.input[0]
     OUTPUT_FILE = args.output_file
-    BACK_MUS = args.background_music
-    BACK_VOL = args.background_volume
-    NEW_TRAC = args.cut_by_this_audio
-    BASE_TRAC = args.cut_by_this_track
-    COMBINE_TRAC = args.cut_by_all_tracks
 
-    SILENT_SPEED = args.silent_speed
-    VIDEO_SPEED = args.video_speed
-
-    if(SILENT_SPEED <= 0 or SILENT_SPEED > 99999):
-        SILENT_SPEED = 99999
-    if(VIDEO_SPEED <= 0 or VIDEO_SPEED > 99999):
-        VIDEO_SPEED = 99999
-
-    HWACCEL = args.hardware_accel
+    if(args.silent_speed <= 0 or args.silent_speed > 99999):
+        args.silent_speed = 99999
+    if(args.video_speed <= 0 or args.video_speed > 99999):
+        args.video_speed = 99999
 
     if(os.path.isdir(INPUT_FILE)):
         # get the file path and date modified so that it can be sorted later.
@@ -159,7 +163,7 @@ if(__name__ == '__main__'):
 
                 INPUTS.append(dic)
 
-        # sort the list by the key 'time'.
+        # Sort the list by the key 'time'.
         newlist = sorted(INPUTS, key=itemgetter('time'), reverse=False)
 
         # then reduce to a list that only has strings.
@@ -183,18 +187,20 @@ if(__name__ == '__main__'):
             os.remove('combine_files.txt')
         else:
             outputDir = INPUT_FILE + '_ALTERED'
-            # create the new folder for all the outputs
+            # Create the new folder for all the outputs.
             try:
                 os.mkdir(outputDir)
             except OSError:
                 rmtree(outputDir)
                 os.mkdir(outputDir)
     else:
+        if(args.combine_files):
+            print('Warning! --combine_files does nothing since input is not a folder.')
         outputDir = ''
         if(os.path.isfile(INPUT_FILE)):
             INPUTS = [INPUT_FILE]
-        # if input is URL, download as mp4 with youtube-dl
         elif(INPUT_FILE.startswith('http://') or INPUT_FILE.startswith('https://')):
+            # If input is a URL, download as mp4 with youtube-dl.
             print('URL detected, using youtube-dl to download from webpage.')
             basename = re.sub(r'\W+', '-', INPUT_FILE)
             cmd = ["youtube-dl", "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
@@ -207,26 +213,34 @@ if(__name__ == '__main__'):
                 OUTPUT_FILE = basename + '_ALTERED.mp4'
         else:
             print('Could not find file:', INPUT_FILE)
-            sys.exit()
+            sys.exit(1)
 
     if(args.preview):
         from scripts.preview import preview
 
         preview(INPUT_FILE, args.silent_threshold, args.zoom_threshold,
-            args.frame_margin, args.sample_rate, VIDEO_SPEED, SILENT_SPEED)
+            args.frame_margin, args.sample_rate, args.video_speed, args.silent_speed)
         sys.exit()
 
     startTime = time.time()
 
-    # all the inputs are stored in the INPUTS variable
-    del INPUT_FILE
-
     for INPUT_FILE in INPUTS:
         dotIndex = INPUT_FILE.rfind('.')
         extension = INPUT_FILE[dotIndex:]
+        if(outputDir != ''):
+            newOutput = os.path.join(outputDir, os.path.basename(INPUT_FILE))
+            print(newOutput)
+        else:
+            newOutput = OUTPUT_FILE
         isAudio = extension in ['.wav', '.mp3', '.m4a']
+        if(isAudio):
+            from scripts.fastAudio import fastAudio
 
-        if(not isAudio):
+            outFile = fastAudio(INPUT_FILE, newOutput, args.silent_threshold, args.frame_margin,
+                args.sample_rate, args.audio_bitrate, args.verbose, args.silent_speed,
+                args.video_speed, True)
+            continue
+        else:
             try:
                 frameRate = getFrameRate(INPUT_FILE)
             except AttributeError:
@@ -245,27 +259,21 @@ if(__name__ == '__main__'):
                 subprocess.call(cmd)
                 INPUT_FILE = TEMP+'/constantVid'+extension
 
-        if(BACK_MUS is None and BACK_VOL != -8):
+        if(args.background_music is None and args.background_volume != -8):
             print('Warning! Background volume specified even though no background music was provided.')
-
-        if(outputDir != ''):
-            newOutput = os.path.join(outputDir, os.path.basename(INPUT_FILE))
-            print(newOutput)
-        else:
-            newOutput = OUTPUT_FILE
 
         if(args.export_to_premiere):
             from scripts.premiere import exportToPremiere
 
             outFile = exportToPremiere(INPUT_FILE, newOutput, args.silent_threshold,
-                args.zoom_threshold, args.frame_margin, args.sample_rate, VIDEO_SPEED,
-                SILENT_SPEED)
+                args.zoom_threshold, args.frame_margin, args.sample_rate, args.video_speed,
+                args.silent_speed)
             continue
 
-        if(BACK_MUS is None and args.zoom_threshold == 2
-            and NEW_TRAC == None and HWACCEL is None and not isAudio):
+        if(args.background_music is None and args.zoom_threshold == 2
+            and args.cut_by_this_audio == None and args.hardware_accel is None):
 
-            if(SILENT_SPEED == 99999 and VIDEO_SPEED == 1):
+            if(args.silent_speed == 99999 and args.video_speed == 1):
                 from scripts.fastVideo import fastVideo
 
                 outFile = fastVideo(INPUT_FILE, newOutput, args.silent_threshold,
@@ -276,15 +284,17 @@ if(__name__ == '__main__'):
 
                 outFile = fastVideoPlus(INPUT_FILE, newOutput, args.silent_threshold,
                     args.frame_margin, args.sample_rate, args.audio_bitrate,
-                    args.verbose, VIDEO_SPEED, SILENT_SPEED, args.cut_by_this_track,
-                    args.keep_tracks_seperate)
+                    args.verbose, args.video_speed, args.silent_speed,
+                    args.cut_by_this_track, args.keep_tracks_seperate)
         else:
             from scripts.originalMethod import originalMethod
 
-            outFile = originalMethod(INPUT_FILE, newOutput, args.frame_rate, args.frame_margin,
-                args.silent_threshold, args.zoom_threshold, args.sample_rate,
-                args.audio_bitrate, SILENT_SPEED, VIDEO_SPEED, args.keep_tracks_seperate, BACK_MUS,
-                BACK_VOL, NEW_TRAC, BASE_TRAC, COMBINE_TRAC, args.verbose, HWACCEL)
+            outFile = originalMethod(INPUT_FILE, newOutput, args.frame_rate,
+                args.frame_margin, args.silent_threshold, args.zoom_threshold,
+                args.sample_rate, args.audio_bitrate, args.silent_speed, args.video_speed,
+                args.keep_tracks_seperate, args.background_music, args.background_volume,
+                args.cut_by_this_audio, args.cut_by_this_track, args.cut_by_all_tracks,
+                args.verbose, args.hardware_accel)
 
     print('Finished.')
     timeLength = round(time.time() - startTime, 2)
---
+++ scripts/fastAudio.py
@@ -0,0 +1,110 @@
+'''scripts/fastAudio.py'''
+
+"""
+This script is for handling audio files ONLY.
+"""
+
+# External libraries
+import numpy as np
+from audiotsm import phasevocoder
+
+# Included functions
+from scripts.readAudio import ArrReader, ArrWriter
+from scripts.usefulFunctions import getAudioChunks, progressBar
+from scripts.wavfile import read, write
+
+# Internal libraries
+import os
+import sys
+import subprocess
+
+def preview(chunks, NEW_SPEED, fps):
+    timeInFrames = 0
+    for chunk in chunks:
+        leng = chunk[1] - chunk[0]
+        if(NEW_SPEED[chunk[2]] < 99999):
+            timeInFrames += leng * (1 / NEW_SPEED[chunk[2]])
+    return timeInFrames / fps
+
+
+def fastAudio(theFile, outFile, silentT, frameMargin, SAMPLE_RATE, audioBit, verbose,
+    silentSpeed, soundedSpeed, needConvert):
+
+    if(not os.path.isfile(theFile)):
+        print('Could not find file:', theFile)
+        sys.exit(1)
+
+    if(outFile == ''):
+        fileName = theFile[:theFile.rfind('.')]
+        outFile = f'{fileName}_ALTERED.wav'
+
+    if(needConvert):
+        # Only print this here so other programs can use this function.
+        print('Running from fastAudio.py')
+
+        import tempfile
+        from shutil import rmtree
+
+        TEMP = tempfile.mkdtemp()
+
+        cmd = ['ffmpeg', '-i', theFile, '-b:a', audioBit, '-ac', '2', '-ar',
+            str(SAMPLE_RATE), '-vn', f'{TEMP}/fastAud.wav']
+        if(not verbose):
+            cmd.extend(['-nostats', '-loglevel', '0'])
+        subprocess.call(cmd)
+
+        theFile = f'{TEMP}/fastAud.wav'
+
+    NEW_SPEED = [silentSpeed, soundedSpeed]
+
+    sampleRate, audioData = read(theFile)
+    chunks = getAudioChunks(audioData, sampleRate, 30, silentT, 2, frameMargin)
+
+    # Get the estimated length of the new audio in frames.
+    hmm = preview(chunks, NEW_SPEED, 30)
+
+    # Get the new length in samples with some extra leeway.
+    estLeng = int((hmm * sampleRate) * 1.5) + int(sampleRate * 2)
+
+    # Create an empty array for the new audio.
+    newAudio = np.zeros((estLeng, 2), dtype=np.int16)
+
+    channels = 2
+    yPointer = 0
+
+    # samples per frame
+    spf = int(sampleRate / 30)
+
+    for chunk in chunks:
+        audioSampleStart = int(chunk[0] / 30 * sampleRate)
+        audioSampleEnd = audioSampleStart + spf * (chunk[1] - chunk[0])
+
+        theSpeed = NEW_SPEED[chunk[2]]
+
+        print(yPointer)
+
+        if(theSpeed != 99999):
+            spedChunk = audioData[audioSampleStart:audioSampleEnd]
+            spedupAudio = np.zeros((0, 2), dtype=np.int16)
+            with ArrReader(spedChunk, channels, sampleRate, 2) as reader:
+                with ArrWriter(spedupAudio, channels, sampleRate, 2) as writer:
+                    phasevocoder(reader.channels, speed=theSpeed).run(
+                        reader, writer
+                    )
+                    spedupAudio = writer.output
+
+            yPointerEnd = yPointer + spedupAudio.shape[0]
+            newAudio[yPointer:yPointerEnd] = spedupAudio
+
+            yPointer = yPointerEnd
+        else:
+            # Speed is too high so skip this section.
+            yPointerEnd = yPointer
+
+    newAudio = newAudio[:yPointer]
+    write(outFile, sampleRate, newAudio)
+
+    if('TEMP' in locals()):
+        rmtree(TEMP)
+    return outFile
+
