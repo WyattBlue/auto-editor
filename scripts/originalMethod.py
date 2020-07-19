e5c729af4d39ecf33ec8ec5de6a130647494cab1
WyattBlue <wyatt@pugranch.com>
2020-07-19 06:17:14 -0400

    changed mac ffmpeg

--- scripts/originalMethod.py
+++ scripts/originalMethod.py
@@ -40,7 +40,7 @@ def getFrameRate(ffmpeg, path):
     return float(matchDict['fps'])
 
 
-def getZooms(chunks, audioFrameCount, hasLoudAudio, frameMargin):
+def getZooms(chunks, audioFrameCount, hasLoudAudio, frameMargin, frameRate):
     zooms = {}
     shouldIncludeFrame = np.zeros((audioFrameCount), dtype=np.uint8)
     hold = False
@@ -214,7 +214,7 @@ def originalMethod(ffmpeg, vidFile, outFile, frameMargin, silentT,
     chunks.append([chunks[-1][1], audioFrameCount, shouldIncludeFrame[i-1]])
     chunks = chunks[1:]
 
-    zooms = getZooms(chunks, audioFrameCount, hasLoudAudio, frameMargin)
+    zooms = getZooms(chunks, audioFrameCount, hasLoudAudio, frameMargin, frameRate)
 
     conwrite('')
 
