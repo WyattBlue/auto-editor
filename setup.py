3c65e062d78a0a302d1755e81bd9d5a5d6c7ee43
WyattBlue <57511737+WyattBlue@users.noreply.github.com>
2020-07-25 14:11:51 -0400

    20w30b

---
+++ setup.py
@@ -0,0 +1,63 @@
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+def find_version():
+    return '0.1.6'
+
+
+# 'setup.py publish' shortcut.
+if(sys.argv[-1] == 'publish'):
+    os.system('rm -rf build')
+    os.system('rm -rf dist')
+    os.system('python3 setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='auto-editor',
+    version=find_version(),
+    description='Auto-Editor: Effort free video editing!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/WyattBlue/auto-editor',
+    author='WyattBlue',
+    author_email='wyattbluesandbox@gmail.com',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'audiotsm2',
+        'youtube_dl',
+        'pillow',
+        'pydub',
+    ],
+    classifiers=[
+        'Topic :: Multimedia :: Video',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: IronPython',
+        'Programming Language :: Python :: Implementation :: Jython',
+    ],
+    entry_points={
+        "console_scripts": ["auto-editor=scripts.__main__:main"]
+    }
+)
