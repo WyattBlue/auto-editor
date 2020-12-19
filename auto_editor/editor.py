5e1c6c5255284e5ee647a04cb21988fe8503ae5b
WyattBlue <wyatt@pugranch.com>
2020-12-19 00:20:16 -0500

    added multi-track abilities for resolve

--- auto_editor/editor.py
+++ auto_editor/editor.py
@@ -197,23 +197,27 @@ def editorXML(myInput: str, temp: str, output, clips, chunks, tracks: int,
         outfile.write('<xmeml version="4">\n')
         outfile.write('\t<sequence>\n')
         outfile.write(f'\t\t<name>{groupName}</name>\n')
+        outfile.write('\t\t<rate>\n')
+        outfile.write(f'\t\t\t<timebase>{timebase}</timebase>\n')
+        outfile.write(f'\t\t\t<ntsc>{ntsc}</ntsc>\n')
+        outfile.write('\t\t</rate>\n')
         outfile.write('\t\t<media>\n')
-        outfile.write('\t\t\t<video>\n')
-        outfile.write('\t\t\t\t<format>\n')
-        outfile.write('\t\t\t\t\t<samplecharacteristics>\n')
-        outfile.write('\t\t\t\t\t\t<rate>\n')
-        outfile.write(f'\t\t\t\t\t\t\t<timebase>{timebase}</timebase>\n')
-        outfile.write(f'\t\t\t\t\t\t\t<ntsc>{ntsc}</ntsc>\n')
-        outfile.write('\t\t\t\t\t\t</rate>\n')
-        outfile.write(f'\t\t\t\t\t\t<width>{width}</width>\n')
-        outfile.write(f'\t\t\t\t\t\t<height>{height}</height>\n')
-        outfile.write(f'\t\t\t\t\t\t<anamorphic>{ana}</anamorphic>\n')
-        outfile.write(f'\t\t\t\t\t\t<pixelaspectratio>{pixelar}</pixelaspectratio>\n')
-        outfile.write('\t\t\t\t\t\t<fielddominance>none</fielddominance>\n')
-        outfile.write(f'\t\t\t\t\t\t<colordepth>{colordepth}</colordepth>\n')
-        outfile.write('\t\t\t\t\t</samplecharacteristics>\n')
-        outfile.write('\t\t\t\t</format>\n')
-        outfile.write('\t\t\t\t<track>\n')
+
+        outfile.write(formatXML(3, '<video>', '\t<format>',
+            '\t\t<samplecharacteristics>',
+            '\t\t\t<rate>',
+            f'\t\t\t\t<timebase>{timebase}</timebase>',
+            f'\t\t\t\t<ntsc>{ntsc}</ntsc>',
+            '\t\t\t</rate>',
+            f'\t\t\t<width>{width}</width>',
+            f'\t\t\t<height>{height}</height>',
+            f'\t\t\t<anamorphic>{ana}</anamorphic>',
+            f'\t\t\t<pixelaspectratio>{pixelar}</pixelaspectratio>',
+            '\t\t\t<fielddominance>none</fielddominance>',
+            f'\t\t\t<colordepth>{colordepth}</colordepth>',
+            '\t\t</samplecharacteristics>',
+            '\t</format>',
+            '\t<track>'))
 
         # Handle clips.
         total = 0
@@ -222,51 +226,47 @@ def editorXML(myInput: str, temp: str, output, clips, chunks, tracks: int,
             total += (clip[1] - clip[0]) / (clip[2] / 100)
             myEnd = int(total)
 
-            outfile.write(f'\t\t\t\t\t<clipitem id="clipitem-{j+1}">\n')
-            outfile.write('\t\t\t\t\t\t<masterclipid>masterclip-2</masterclipid>\n')
-            outfile.write(f'\t\t\t\t\t\t<name>{name}</name>\n')
-            outfile.write(f'\t\t\t\t\t\t<start>{myStart}</start>\n')
-            outfile.write(f'\t\t\t\t\t\t<end>{myEnd}</end>\n')
-            outfile.write(f'\t\t\t\t\t\t<in>{int(clip[0] / (clip[2] / 100))}</in>\n')
-            outfile.write(f'\t\t\t\t\t\t<out>{int(clip[1] / (clip[2] / 100))}</out>\n')
+            outfile.write(formatXML(5, f'<clipitem id="clipitem-{j+1}">',
+                '\t<masterclipid>masterclip-2</masterclipid>',
+                f'\t<name>{name}</name>',
+                f'\t<start>{myStart}</start>',
+                f'\t<end>{myEnd}</end>',
+                f'\t<in>{int(clip[0] / (clip[2] / 100))}</in>',
+                f'\t<out>{int(clip[1] / (clip[2] / 100))}</out>'))
 
             if(j == 0):
-                outfile.write('\t\t\t\t\t\t<file id="file-1">\n')
-                outfile.write(f'\t\t\t\t\t\t\t<name>{name}</name>\n')
-                outfile.write(f'\t\t\t\t\t\t\t<pathurl>{pathurl}</pathurl>\n')
-                outfile.write('\t\t\t\t\t\t\t<rate>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t<timebase>{timebase}</timebase>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t<ntsc>{ntsc}</ntsc>\n')
-                outfile.write('\t\t\t\t\t\t\t</rate>\n')
-                outfile.write('\t\t\t\t\t\t\t<media>\n')
-                outfile.write('\t\t\t\t\t\t\t\t<video>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t<samplecharacteristics>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t\t<rate>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t\t<timebase>{timebase}</timebase>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t\t<ntsc>{ntsc}</ntsc>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t\t</rate>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t<width>{width}</width>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t<height>{height}</height>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t<anamorphic>{ana}</anamorphic>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t<pixelaspectratio>{pixelar}</pixelaspectratio>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t\t<fielddominance>none</fielddominance>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t</samplecharacteristics>\n')
-                outfile.write('\t\t\t\t\t\t\t\t</video>\n')
-                outfile.write('\t\t\t\t\t\t\t\t<audio>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t<samplecharacteristics>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t<depth>{depth}</depth>\n')
-                outfile.write(f'\t\t\t\t\t\t\t\t\t\t<samplerate>{sr}</samplerate>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t</samplecharacteristics>\n')
-                outfile.write('\t\t\t\t\t\t\t\t\t<channelcount>2</channelcount>\n')
-                outfile.write('\t\t\t\t\t\t\t\t</audio>\n')
-                outfile.write('\t\t\t\t\t\t\t</media>\n')
-                outfile.write('\t\t\t\t\t\t</file>\n')
+                outfile.write(formatXML(6, '<file id="file-1">',
+                    f'\t<name>{name}</name>',
+                    f'\t<pathurl>{pathurl}</pathurl>',
+                    '\t<rate>',
+                    f'\t\t<timebase>{timebase}</timebase>',
+                    f'\t\t<ntsc>{ntsc}</ntsc>',
+                    '\t</rate>',
+                    f'\t<duration>{duration}</duration>',
+                    '\t<media>', '\t\t<video>',
+                    '\t\t\t<samplecharacteristics>',
+                    '\t\t\t\t<rate>',
+                    f'\t\t\t\t\t<timebase>{timebase}</timebase>',
+                    f'\t\t\t\t\t<ntsc>{ntsc}</ntsc>',
+                    '\t\t\t\t</rate>',
+                    f'\t\t\t\t<width>{width}</width>',
+                    f'\t\t\t\t<height>{height}</height>',
+                    f'\t\t\t\t<anamorphic>{ana}</anamorphic>',
+                    f'\t\t\t\t<pixelaspectratio>{pixelar}</pixelaspectratio>',
+                    '\t\t\t\t<fielddominance>none</fielddominance>',
+                    '\t\t\t</samplecharacteristics>',
+                    '\t\t</video>', '\t\t<audio>',
+                    '\t\t\t<samplecharacteristics>',
+                    f'\t\t\t\t<depth>{depth}</depth>',
+                    f'\t\t\t\t<samplerate>{sr}</samplerate>',
+                    '\t\t\t</samplecharacteristics>',
+                    '\t\t\t<channelcount>2</channelcount>',
+                    '\t\t</audio>', '\t</media>', '</file>'))
             else:
                 outfile.write('\t\t\t\t\t\t<file id="file-1"/>\n')
 
             if(clip[2] != 100):
                 outfile.write(speedup(clip[2]))
-
 
             # Linking for video blocks
             for i in range(max(3, tracks + 1)):
@@ -284,17 +284,18 @@ def editorXML(myInput: str, temp: str, output, clips, chunks, tracks: int,
                 if(i > 0):
                     outfile.write('\t\t\t\t\t\t\t<groupindex>1</groupindex>\n')
                 outfile.write('\t\t\t\t\t\t</link>\n')
+
             outfile.write('\t\t\t\t\t</clipitem>\n')
-        outfile.write('\t\t\t\t</track>\n')
-        outfile.write('\t\t\t</video>\n')
-        outfile.write('\t\t\t<audio>\n')
-        outfile.write('\t\t\t\t<numOutputChannels>2</numOutputChannels>\n')
-        outfile.write('\t\t\t\t<format>\n')
-        outfile.write('\t\t\t\t\t<samplecharacteristics>\n')
-        outfile.write(f'\t\t\t\t\t\t<depth>{depth}</depth>\n')
-        outfile.write(f'\t\t\t\t\t\t<samplerate>{sr}</samplerate>\n')
-        outfile.write('\t\t\t\t\t</samplecharacteristics>\n')
-        outfile.write('\t\t\t\t</format>\n')
+
+        # End Video; Start Audio
+        outfile.write(formatXML(3, '\t</track>', '</video>', '<audio>',
+            '\t<numOutputChannels>2</numOutputChannels>',
+            '\t<format>',
+            '\t\t<samplecharacteristics>',
+            f'\t\t\t<depth>{depth}</depth>',
+            f'\t\t\t<samplerate>{sr}</samplerate>',
+            '\t\t</samplecharacteristics>',
+            '\t</format>'))
 
         # Audio Clips
         for t in range(tracks):
@@ -323,23 +324,22 @@ def editorXML(myInput: str, temp: str, output, clips, chunks, tracks: int,
                 outfile.write(f'\t\t\t\t\t\t<out>{int(clip[1] / (clip[2] / 100))}</out>\n')
 
                 if(t > 0):
-                    outfile.write(f'\t\t\t\t\t\t<file id="file-{t+1}">\n')
-                    outfile.write(f'\t\t\t\t\t\t\t<name>{name}{t}</name>\n')
-                    outfile.write(f'\t\t\t\t\t\t\t<pathurl>{trackurls[t]}</pathurl>\n')
-                    outfile.write('\t\t\t\t\t\t\t<rate>\n')
-                    outfile.write(f'\t\t\t\t\t\t\t\t<timebase>{timebase}</timebase>\n')
-                    outfile.write(f'\t\t\t\t\t\t\t\t<ntsc>{ntsc}</ntsc>\n')
-                    outfile.write('\t\t\t\t\t\t\t</rate>\n')
-                    outfile.write('\t\t\t\t\t\t\t<media>\n')
-                    outfile.write('\t\t\t\t\t\t\t\t<audio>\n')
-                    outfile.write('\t\t\t\t\t\t\t\t\t<samplecharacteristics>\n')
-                    outfile.write(f'\t\t\t\t\t\t\t\t\t\t<depth>{depth}</depth>\n')
-                    outfile.write(f'\t\t\t\t\t\t\t\t\t\t<samplerate>{sr}</samplerate>\n')
-                    outfile.write('\t\t\t\t\t\t\t\t\t</samplecharacteristics>\n')
-                    outfile.write('\t\t\t\t\t\t\t\t\t<channelcount>2</channelcount>\n')
-                    outfile.write('\t\t\t\t\t\t\t\t</audio>\n')
-                    outfile.write('\t\t\t\t\t\t\t</media>\n')
-                    outfile.write('\t\t\t\t\t\t</file>\n')
+                    # Define arbitrary file
+                    outfile.write(formatXML(6, f'<file id="file-{t+1}">',
+                        f'\t<name>{name}{t}</name>',
+                        f'\t<pathurl>{trackurls[t]}</pathurl>',
+                        '\t<rate>',
+                        f'\t\t<timebase>{timebase}</timebase>',
+                        f'\t\t<ntsc>{ntsc}</ntsc>',
+                        '\t</rate>',
+                        '\t<media>',
+                        '\t\t<audio>',
+                        '\t\t\t<samplecharacteristics>',
+                        f'\t\t\t\t<depth>{depth}</depth>',
+                        f'\t\t\t\t<samplerate>{sr}</samplerate>',
+                        '\t\t\t</samplecharacteristics>',
+                        '\t\t\t<channelcount>2</channelcount>',
+                        '\t\t</audio>', '\t</media>', '</file>'))
                 else:
                     outfile.write(f'\t\t\t\t\t\t<file id="file-{t+1}"/>\n')
                 outfile.write('\t\t\t\t\t\t<sourcetrack>\n')
