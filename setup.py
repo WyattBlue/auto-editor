d4570aa3a5cbdcc42c130e1de6a282dc976715e0
WyattBlue <wyatt@pugranch.com>
2021-01-29 23:12:49 -0500

    bumped up version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import sys
 from setuptools import setup, find_packages
 
 def pip_version():
-    return '21.4.1'
+    return '21.4.2'
 
 if(sys.argv[-1] == 'publish'):
     os.system('rm -rf build')
