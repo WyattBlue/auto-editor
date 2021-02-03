2323320c55d004d0b5a68407e814dd3a93501081
WyattBlue <wyatt@pugranch.com>
2021-02-03 15:58:19 -0500

    changed seperator for Windows

---
+++ auto_editor/renderWaveform.py
@@ -0,0 +1,7 @@
+
+def renderWaveform(inFile, outFile, ffmpeg, size: str):
+
+    ffmpeg.run(['-i', inFile, '-filter_complex',
+        f'aformat=channel_layouts=mono,showwavespic=s={size}', '-frames:v', '1', outFile])
+
+
