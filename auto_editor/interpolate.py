e5391b11c4b61d439babefab66007baf3f0a7797
WyattBlue <wyatt@pugranch.com>
2021-02-11 21:00:57 -0500

    working on zoom stuff

--- auto_editor/interpolate.py
+++ auto_editor/interpolate.py
@@ -2,7 +2,6 @@
 
 def linear(x, y, n) -> list:
     b = [x]
-
     step = (y - x) / n
     incre = x
     for i in range(n - 2):
@@ -11,3 +10,10 @@ def linear(x, y, n) -> list:
 
     b.append(y)
     return b
+
+
+def interpolate(x, y, n, log, method='linear') -> list:
+    if(method == 'linear'):
+        return linear(x, y, n)
+    else:
+        log.error(f"Method: {method} isn't implemented.")
