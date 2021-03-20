8c0b3297814e458ee884378327d6e16072cfa9d8
WyattBlue <wyatt@pugranch.com>
2021-03-20 06:14:34 -0500

    fixing -exf logic

--- auto_editor/editor.py
+++ auto_editor/editor.py
@@ -56,60 +56,111 @@ def fixUrl(path: str, resolve: bool) -> str:
 def fcpXML(myInput: str, temp: str, output, ffprobe, clips, chunks, tracks: int,
     sampleRate, audioFile, fps, log):
 
-    duration = chunks[len(chunks) - 1][1]
-    pathurl = 'file:///' + os.path.abspath(myInput)
+    pathurl = 'file://' + os.path.abspath(myInput)
     name = os.path.splitext(os.path.basename(myInput))[0]
 
-    def fraction(inp: float) -> str:
-        num, dem = inp.as_integer_ratio()
-        if(num == 0):
+    def fraction(inp, fps) -> str:
+        from fractions import Fraction
+
+        if(inp == 0):
             return '0s'
-        num *= 1000
-        dem *= 1000
+
+        if(isinstance(inp, float)):
+            inp = Fraction(inp)
+        if(isinstance(fps, float)):
+            fps = Fraction(fps)
+
+
+        frac = Fraction(inp, fps).limit_denominator()
+        num = frac.numerator
+        dem = frac.denominator
+
+        if(dem < 3000):
+            factor = int(3000 / dem)
+            print(3000 / dem)
+            print(f'{num}/{dem}')
+            num *= factor
+            dem *= factor
+            print(f'{num}/{dem}')
+
         return f'{num}/{dem}s'
 
     if(not audioFile):
         width, height = ffprobe.getResolution(myInput).split('x')
+        total_dur = ffprobe.getDuration(myInput)
+        if(total_dur == 'N/A'):
+            total_dur = ffprobe.pipe(['-show_entries', 'format=duration', '-of',
+                'default=noprint_wrappers=1:nokey=1', myInput]).strip()
     else:
         width, height = '1920', '1080'
+        total_dur = ffprobe.getAudioDuration(myInput)
+    total_dur = float(total_dur) * fps
 
     with open(output, 'w', encoding='utf-8') as outfile:
+
+        frame_duration = fraction(1, fps)
+        new_duration = fraction(chunks[len(chunks) - 1][1], fps)
+
         outfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         outfile.write('<!DOCTYPE fcpxml>\n\n')
         outfile.write('<fcpxml version="1.9">\n')
         outfile.write('\t<resources>\n')
         outfile.write(f'\t\t<format id="r1" name="FFVideoFormat{height}p{fps}" '\
-            f'frameDuration="100/3000s" width="{width}" height="{height}"'\
+            f'frameDuration="{frame_duration}" width="{width}" height="{height}"'\
             ' colorSpace="1-1-1 (Rec. 709)"/>\n')
 
         outfile.write(f'\t\t<asset id="r2" name="{name}" start="0s" '\
-            'duration="3816000/90000s" hasVideo="1" format="r1" hasAudio="1" '\
+            'hasVideo="1" format="r1" hasAudio="1" '\
             f'audioSources="1" audioChannels="2" audioRate="{sampleRate}">\n')
 
         outfile.write(f'\t\t\t<media-rep kind="original-media" '\
             f'src="{pathurl}"></media-rep>\n')
         outfile.write('\t\t</asset>\n')
         outfile.write('\t</resources>\n')
-        outfile.write('\t<library location="file:///Users/wyattblue/Movies/Untitled.fcpbundle/">\n')
-        outfile.write('\t\t<event name="3-16-21">\n')
+        outfile.write('\t<library>\n')
+        outfile.write('\t\t<event name="auto-editor output">\n')
         outfile.write(f'\t\t\t<project name="{name}">\n')
         outfile.write(formatXML(4,
-            f'<sequence duration="{fraction(duration)}" format="r1" tcStart="0s" tcFormat="NDF" '\
+            f'<sequence duration="{new_duration}" format="r1" tcStart="0s" tcFormat="NDF" '\
             'audioLayout="stereo" audioRate="48k">',
-            '\t<spine>'))
+            '\t<spine>')
+        )
 
-        total = 0
+        last_dur = 0
+
         for j, clip in enumerate(clips):
-            total += (clip[1] - clip[0])
-            off = fraction(clip[0] + total)
+            clip_dur = (clip[1] - clip[0]) / (clip[2] / 100)
+            dur = fraction(clip_dur, fps)
 
-            dur = fraction(clip[1] - clip[0])
-            if(j == 0):
-                outfile.write(formatXML(6, f'<asset-clip name="{name}" offset="{total}" ref="r2"'\
-                f' duration="{dur}" audioRole="dialogue" tcFormat="NDF"/>'))
+            close = '/' if clip[2] == 100 else ''
+
+            if(last_dur == 0):
+                outfile.write(formatXML(6, f'<asset-clip name="{name}" offset="0s" ref="r2"'\
+                f' duration="{dur}" audioRole="dialogue" tcFormat="NDF"{close}>'))
             else:
-                outfile.write(formatXML(6, f'<asset-clip name="{name}" offset="{total}" ref="r2"'\
-                f' duration="{dur}" audioRole="dialogue" tcFormat="NDF"/>'))
+                start = fraction(clip[0], fps)
+                off = fraction(last_dur, fps)
+                outfile.write(formatXML(6,
+                    f'<asset-clip name="{name}" offset="{off}" ref="r2"'\
+                    f' duration="{dur}" start="{start}" audioRole="dialogue" tcFormat="NDF"{close}>',
+                ))
+
+            if(clip[2] != 100):
+                # See "Time Maps" in developer.apple.com/library/archive/documentation/FinalCutProX/Reference/FinalCutProXXMLFormat/StoryElements/StoryElements.html
+
+                frac_total = fraction(total_dur, fps)
+                total_dur_divided_by_speed = fraction((total_dur) / (clip[2] / 100), fps)
+
+
+                outfile.write(formatXML(6,
+                    '\t<timeMap>',
+                    '\t\t<timept time="0s" value="0s" interp="smooth2"/>',
+                    f'\t\t<timept time="{total_dur_divided_by_speed}" value="{frac_total}" interp="smooth2"/>',
+                    '\t</timeMap>',
+                    '</asset-clip>'
+                ))
+
+            last_dur += clip_dur
 
         outfile.write('\t\t\t\t\t</spine>\n')
         outfile.write('\t\t\t\t</sequence>\n')
