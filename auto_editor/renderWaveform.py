f6941ee750d3f56ff27a694464d9ef295b069336
WyattBlue <57511737+WyattBlue@users.noreply.github.com>
2021-04-04 14:24:24 -0500

    deleted leftover file
    This file was originally going to be used for rendering a waveform
    for a gui frontend, but it was never utilized.

--- auto_editor/renderWaveform.py
+++
@@ -1,6 +0,0 @@
-'''renderWaveform.py'''
-
-def renderWaveform(inFile, outFile, ffmpeg, size: str):
-
-    ffmpeg.run(['-i', inFile, '-filter_complex',
-        f'aformat=channel_layouts=mono,showwavespic=s={size}', '-frames:v', '1', outFile])
