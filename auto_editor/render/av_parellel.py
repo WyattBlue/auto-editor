caef26cb2a98db32b4fa08b3dda42128b5c05a58
WyattBlue <wyattblue@auto-editor.com>
2021-09-16 11:31:08 -0500

    get video rendering working on multiprocess
    Making the current chunks and concatenating still need to be done.

--- auto_editor/render/av_parellel.py
+++ auto_editor/render/av_parellel.py
@@ -52,33 +52,37 @@ def split_chunks(chunks, split):
     # chunks = [[0, 5, 1], [5, 10, 2], [10, 15, 3], [15, 20, 4]]
     # print(split_chunks(chunks, split=2))
 
-def render_av(ffmpeg, inp, args, chunks, speeds, fps, has_vfr, temp, log):
-    totalFrames = chunks[len(chunks) - 1][1]
+def multi_render(pickle_data):
 
-    SPLIT_DURATION = 5 # Split the video into 5 second chunks
+    try:
+        import av
 
-    total_seconds = int(totalFrames / fps)
+        path = pickle_data['path']
 
-    vid_space = os.path.join(temp, 'video')
-    os.mkdir(vid_space)
+        log = pickle_data['log']
+        fps = pickle_data['fps']
+        n = pickle_data['n']
+        args = pickle_data['args']
+        speeds = pickle_data['speeds']
+        chunks = pickle_data['chunks']
+        temp = pickle_data['temp']
+        ffmpeg = pickle_data['ffmpeg']
 
-    vid_files = []
-    n = 0
-    for i in range(0, total_seconds, SPLIT_DURATION):
-        print(i)
-        vid_file = os.path.join(vid_space, '{}{}'.format(n, inp.ext))
-        ffmpeg.run(['-i', inp.path, '-vcodec', 'copy', '-ss', str(i), '-to',
-            str(i + SPLIT_DURATION), vid_file])
+        class AttrDict(dict):
+            def __init__(self, *args, **kwargs):
+                super(AttrDict, self).__init__(*args, **kwargs)
+                self.__dict__ = self
 
-        vid_files.append(vid_file)
-        n += 1
+        inp = AttrDict(pickle_data['inp'])
 
-    def render_new_video(path, inp):
+        inp.path = path
+        print(inp.path)
+
         input_ = av.open(path)
         pix_fmt = input_.streams.video[0].pix_fmt
 
-        if(not pix_fmt_allowed(pix_fmt)):
-            throw_pix_fmt(inp, pix_fmt, log)
+        # # if(not pix_fmt_allowed(pix_fmt)):
+        # #     throw_pix_fmt(inp, pix_fmt, log)
 
         inputVideoStream = input_.streams.video[0]
         inputVideoStream.thread_type = 'AUTO'
@@ -94,8 +98,8 @@ def render_av(ffmpeg, inp, args, chunks, speeds, fps, has_vfr, temp, log):
 
         correct_ext = '.mp4' if inp.ext == '.gif' else inp.ext
 
-        spedup = os.path.join(temp, 'spedup{}'.format(correct_ext))
-        scale = os.path.join(temp, 'scale{}'.format(correct_ext))
+        spedup = os.path.join(temp, 'video', 'sped{}{}'.format(n, correct_ext))
+        scale = os.path.join(temp, 'video', 'scale{}{}'.format(n, correct_ext))
 
         if(args.scale != 1):
             cmd.extend(['-vf', 'scale=iw*{}:ih*{}'.format(args.scale, args.scale), scale])
@@ -135,6 +139,48 @@ def render_av(ffmpeg, inp, args, chunks, speeds, fps, has_vfr, temp, log):
         if(args.scale != 1):
             scale_to_sped(ffmpeg, spedup, scale, inp, args, temp)
 
+        print('hi')
+        import time
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        raise e
+
+
+def render_av(ffmpeg, inp, args, chunks, speeds, fps, has_vfr, temp, log):
+    totalFrames = chunks[len(chunks) - 1][1]
+
+    SPLIT_DURATION = 5 # Split the video into 5 second chunks
+
+    total_seconds = int(totalFrames / fps)
+
+    vid_space = os.path.join(temp, 'video')
+    os.mkdir(vid_space)
+
+    split_prog = ProgressBar(total_seconds, 'Splitting video',
+        args.machine_readable_progress, args.no_progress)
+
+    pickle_data = []
+    n = 0
+    for i in range(0, total_seconds, SPLIT_DURATION):
+        vid_file = os.path.join(vid_space, '{}{}'.format(n, inp.ext))
+        ffmpeg.run(['-i', inp.path, '-vcodec', 'copy', '-ss', str(i), '-to',
+            str(i + SPLIT_DURATION), vid_file])
+
+        pickle_data.append({
+            'path': vid_file,
+            'log': log,
+            'n': n,
+            'inp': inp.__dict__,
+            'args': args,
+            'chunks': [[0, 75, 1], [75, 150, 0]],
+            'speeds': [99999, 1],
+            'fps': fps,
+            'temp': temp,
+            'ffmpeg': ffmpeg,
+            })
+        split_prog.tick(i)
+        n += 1
 
     # Using multi-processing, which is better for CPU bound tasks.
     # You can try threading, which is better for IO and networking bound
@@ -142,7 +188,7 @@ def render_av(ffmpeg, inp, args, chunks, speeds, fps, has_vfr, temp, log):
 
     import concurrent.futures
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(render_new_video, vid_files, inp)
+        executor.map(multi_render, pickle_data)
 
 
     # TODO: add video concatenating script
