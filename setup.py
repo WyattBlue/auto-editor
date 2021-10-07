4844227b6072f18865328ddaddb41ae13539653c
WyattBlue <wyattblue@auto-editor.com>
2021-10-07 14:16:54 -0500

    add desc subcommand

--- setup.py
+++ setup.py
@@ -109,6 +109,7 @@ setup(
         "console_scripts": [
             "auto-editor=auto_editor.__main__:main",
             "aecreate=auto_editor.subcommands.create:main",
+            "aedesc=auto_editor.subcommands.desc:main",
             "aeinfo=auto_editor.subcommands.info:main",
             "aesubdump=auto_editor.subcommands.subdump:main",
             "aegrep=auto_editor.subcommands.grep:main",
