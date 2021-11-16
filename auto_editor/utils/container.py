2d6fbc572fc43f04b6c10a32603766a25c7772d9
WyattBlue <wyattblue@auto-editor.com>
2021-11-16 00:19:47 -0500

    Add more container information
     - Add sample rate container check
     - Simplify how exporting media works. (fixes some export options not
    being applied in some cases)

--- auto_editor/utils/container.py
+++ auto_editor/utils/container.py
@@ -28,38 +28,57 @@ wav_formats = [
 ]
 
 containers = {
+    'apng': {
+        'name': 'Animated Portable Network Graphics',
+        'allow_video': True,
+        'max_video_streams': 1,
+        'vcodecs': ['apng'],
+        'strict': True,
+    },
     'gif': {
+        'name': 'Graphics Interchange Format',
         'allow_video': True,
         'max_video_streams': 1,
         'vcodecs': ['gif'],
+        'strict': True,
     },
     'aac': {
+        'name': 'Advanced Audio Coding',
         'allow_audio': True,
         'max_audio_streams': 1,
         'acodecs': ['aac'],
+        'strict': True,
     },
     'adts': {
+        'name': 'Advanced Audio Coding',
         'allow_audio': True,
         'max_audio_streams': 1,
         'acodecs': ['aac'],
+        'strict': True,
     },
     'wav': {
+        'name': 'Waveform Audio File Format',
         'allow_audio': True,
         'max_audio_streams': 1,
         'acodecs': wav_formats,
+        'strict': True,
     },
     'mp3': {
+        'name': 'MP3 / MPEG-2 Audio Layer 3',
         'allow_audio': True,
         'max_audio_streams': 1,
         'acodecs': ['mp3'],
+        'strict': True,
     },
     'opus': {
+        'name': 'Opus',
         'allow_audio': True,
     },
     'oga': {
         'allow_audio': True,
     },
     'flac': {
+        'name': 'Free Lossless Audio Codec',
         'allow_audio': True,
         'max_audio_streams': 1,
         'acodecs': ['flac'],
@@ -79,14 +98,83 @@ containers = {
         'acodecs': ['opus', 'vorbis'],
     },
     'webm': {
+        'name': 'WebM',
         'allow_video': True,
         'allow_audio': True,
         'allow_subtitle': True,
         'vcodecs': ['vp9', 'vp8', 'av1', 'libaom-av1'],
         'acodecs': ['opus', 'vorbis'],
     },
-    'h264': {
+    'srt': {
+        'name': 'SubRip Text / Subtitle Resource Tracks',
+        'allow_subtitle': True,
+        'scodecs': ['srt'],
+        'max_subtitle_streams': 1,
+        'strict': True,
+    },
+    'vtt': {
+        'name': 'Web Video Text Track',
+        'allow_subtitle': True,
+        'scodecs': ['webvtt'],
+        'max_subtitle_streams': 1,
+        'strict': True,
+    },
+    'ass': {
+        'name': 'SubStation Alpha',
+        'allow_subtitle': True,
+        'scodecs': ['ass', 'ssa'],
+        'max_subtitle_streams': 1,
+        'strict': True,
+    },
+    'ssa': {
+        'name': 'SubStation Alpha',
+        'allow_subtitle': True,
+        'scodecs': ['ass', 'ssa'],
+        'max_subtitle_streams': 1,
+        'strict': True,
+    },
+    'avi': {
+        'name': 'Audio Video Interleave',
         'allow_video': True,
+        'allow_audio': True,
+        'vcodecs': ['mpeg4'],
+        'acodecs': ['mp3'],
+    },
+    'wmv': {
+        'name': 'Windows Media Video',
+        'allow_video': True,
+        'allow_audio': True,
+        'vcodecs': ['msmpeg4v3'],
+        'acodecs': ['wmav2'],
+    },
+    'h264': {
+        'name': 'H.264 / Advanced Video Coding (AVC) / MPEG-4 Part 10',
+        'allow_video': True,
+        'vcodecs': ['h264', 'mpeg4', 'hevc'],
+    },
+    'h265': {
+        'name': 'H.265 / High Efficiency Video Coding (HEVC) / MPEG-H Part 2',
+        'allow_video': True,
+        'vcodecs': ['hevc', 'mpeg4', 'h264'],
+    },
+    'hevc': {
+        'name': 'H.265 / High Efficiency Video Coding (HEVC) / MPEG-H Part 2',
+        'allow_video': True,
+        'vcodecs': ['hevc', 'mpeg4', 'h264'],
+    },
+    'mp4': {
+        'name': 'MP4 / MPEG-4 Part 14',
+        'allow_video': True,
+        'allow_audio': True,
+        'allow_subtitle': True,
+    },
+    'swf': {
+        'name': 'ShockWave Flash / Small Web Format',
+        'allow_video': True,
+        'allow_audio': True,
+        'vcodecs': ['flv1'],
+        'acodecs': ['mp3'],
+        'samplerate': [44100, 22050, 11025],
     },
     'not_in_here': {
         'allow_video': True,
@@ -94,14 +182,17 @@ containers = {
         'allow_subtitle': True,
     },
     'default': {
+        'name': None,
         'allow_video': False,
         'allow_audio': False,
         'allow_subtitle': False,
         'max_video_streams': None,
         'max_audio_streams': None,
+        'max_subtitle_streams': None,
         'vcodecs': None,
         'acodecs': None,
+        'scodecs': None,
+        'strict': False, # There may be more valid codecs than ones listed.
+        'samplerate': None, # Any samplerate is allowed.
     },
 }
-
-# Any container not listed is assumed to accept anything.
