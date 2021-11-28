31d4d85830abb2b8659ec524f5a469dc8f54d282
WyattBlue <wyattblue@auto-editor.com>
2021-11-28 00:31:38 -0500

    Improve preview

--- auto_editor/preview.py
+++ auto_editor/preview.py
@@ -6,26 +6,35 @@ from datetime import timedelta
 # Included Libraries
 from auto_editor.utils.func import get_new_length
 
-def printTimeFrame(title, frames, fps):
+def display_length(secs):
+    # display length
+    if(secs < 0):
+        return '-' + str(timedelta(seconds=round(abs(secs))))
+    return str(timedelta(seconds=round(secs)))
+
+def time_frame(title, frames, fps):
     in_sec = round(frames / fps, 1)
-    fps = round(fps)
-    if(in_sec < 1):
-        minutes = '{}/{} frames'.format(int(frames), fps)
-    else:
-        minutes = timedelta(seconds=round(in_sec))
+    minutes = timedelta(seconds=round(in_sec))
     print('{}: {} secs ({})'.format(title, in_sec, minutes))
 
 
 def preview(inp, chunks, speeds, log):
     fps = 30 if inp.fps is None else float(inp.fps)
 
-    old_time = chunks[len(chunks)-1][1]
-    print('')
-    printTimeFrame('Old length', old_time, fps)
+    log.conwrite('')
 
+    old_length = chunks[-1][1] / fps
     new_length = get_new_length(chunks, speeds, fps)
-    printTimeFrame('New length', new_length * fps, fps)
-    print('')
+
+    diff = new_length - old_length
+
+    print('\nlength:\n - change: ({}) 100% -> ({}) {}%\n - diff: ({}) {}%'.format(
+        display_length(old_length),
+        display_length(new_length),
+        round((new_length / old_length) * 100, 2),
+        display_length(diff),
+        round((diff / old_length) * 100, 2),
+    ))
 
     clips = 0
     cuts = 0
@@ -42,22 +51,22 @@ def preview(inp, chunks, speeds, log):
             leng = chunk[1] - chunk[0]
             cut_lens.append(leng)
 
-    print('Number of clips: {}'.format(clips))
+    print('clips: {}'.format(clips))
     if(len(clip_lens) == 1):
-        printTimeFrame('Clip length', clip_lens[0], fps)
+        time_frame(' - clip length', clip_lens[0], fps)
     else:
-        printTimeFrame('Smallest clip length', min(clip_lens), fps)
-        printTimeFrame('Largest clip length', max(clip_lens), fps)
-        printTimeFrame('Average clip length', sum(clip_lens) / len(clip_lens), fps)
-        print('\nNumber of cuts: {}'.format(cuts))
+        time_frame(' - smallest', min(clip_lens), fps)
+        time_frame(' - largest', max(clip_lens), fps)
+        time_frame(' - average', sum(clip_lens) / len(clip_lens), fps)
+        print('cuts: {}'.format(cuts))
 
     if(cut_lens != []):
         if(len(cut_lens) == 1):
-            printTimeFrame('Cut length', cut_lens[0], fps)
+            time_frame(' - cut length', cut_lens[0], fps)
         else:
-            printTimeFrame('Smallest cut length', min(cut_lens), fps)
-            printTimeFrame('Largest cut length', max(cut_lens), fps)
-            printTimeFrame('Average cut length', sum(cut_lens) / len(cut_lens), fps)
+            time_frame(' - smallest', min(cut_lens), fps)
+            time_frame(' - largest', max(cut_lens), fps)
+            time_frame(' - average', sum(cut_lens) / len(cut_lens), fps)
         print('')
 
     log.debug('Chunks: {}'.format(chunks))
