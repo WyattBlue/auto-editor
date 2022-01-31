457e21080f911cd743d821d1d1e152a8cfccf2cc
WyattBlue <wyattblue@auto-editor.com>
2022-01-31 13:45:06 -0500

    Use dataclass

--- auto_editor/utils/effects.py
+++ auto_editor/utils/effects.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict, fields
+
 from PIL import Image, ImageDraw, ImageFont, ImageChops
 
 def _apply_anchor(x, y, width, height, anchor):
@@ -14,7 +16,7 @@ def _apply_anchor(x, y, width, height, anchor):
     # Pillow uses 'tl' by default
     return x, y
 
-class Effect():
+class Effect:
     __slots__ = ('all', 'sheet', '_vars', 'log', 'background', 'width', 'height')
     def __init__(self, args, log, _vars):
         self.all = []
@@ -46,110 +48,98 @@ class Effect():
 
         return _type(val)
 
-    def set_timing(self, effect, effect_index):
-        start = effect.pop('start')
-        dur = effect.pop('dur')
+    def set_timing(self, obj, index):
+        start = self._values(obj.start, int)
+        dur = self._values(obj.dur, int)
 
-        start = self._values(start[1], int)
-        dur = self._values(dur[1], int)
-
-        if(dur < 1):
+        if dur < 1:
             self.log.error(f"dur's value must be greater than 0. Was '{dur}'.")
 
         for i in range(start, start + dur, 1):
-            if(i in self.sheet):
-                self.sheet[i].append(effect_index)
+            if i in self.sheet:
+                self.sheet[i].append(index)
             else:
-                self.sheet[i] = [effect_index]
-
-        return effect
-
-    def set_all(self, effect):
-        this_type = effect['type']
-        new_effect = {'type': this_type}
-
-        for name, my_tup in effect.items():
-            if(name != 'type'):
-                _type, val = my_tup
-                if(isinstance(val, str) and val.startswith('{') and val.endswith('}')):
-                    new_effect[name] = new_effect[val[1:-1]]
-                else:
-                    new_effect[name] = self._values(val, _type)
-
-        return new_effect
+                self.sheet[i] = [index]
 
     def resolve(self, args):
         self.width = self._vars['width']
         self.height = self._vars['height']
 
-        effect_list = args.add_text + args.add_rectangle + args.add_ellipse + args.add_image
+        pool = args.add_text + args.add_rectangle + args.add_ellipse + args.add_image
 
-        for i, my_effect in enumerate(effect_list):
-            effect = self.set_timing(my_effect, i)
-            effect = self.set_all(effect)
+        for i, obj in enumerate(pool):
+
+            if isinstance(obj, str):
+                raise TypeError('obj is str')
+
+            dic_value = {}
+            for k, v in asdict(obj).items():
+                dic_value[k] = v
+
+            dic_type = {}
+            for field in fields(obj):
+                dic_type[field.name] = field.type
+
+            # Convert to the correct types
+            for k, _type in dic_type.items():
+                obj.__setattr__(k, self._values(dic_value[k], _type))
+
+            self.set_timing(obj, i)
 
             # Change font attr to ImageFont Obj. (must have font and size attr)
-            if('font' in effect):
-                if(effect['font'] == 'default'):
-                    try:
-                        effect['font'] = ImageFont.truetype(effect['font'], effect['size'])
-                    except OSError:
-                        effect['font'] = ImageFont.load_default()
-                else:
-                    try:
-                        effect['font'] = ImageFont.truetype(effect['font'], effect['size'])
-                    except OSError:
-                        self.log.error(f"Font '{effect['font']}' not found.")
+            if obj._type == 'text':
+                try:
+                    obj.font = ImageFont.truetype(obj.font, obj.size)
+                except OSError:
+                    if obj.font == 'default':
+                        obj.font = ImageFont.load_default()
+                    else:
+                        self.log.error(f"Font '{obj.font}' not found.")
 
-            if(effect['type'] == 'image'):
-                source = Image.open(effect['source'])
+            if obj._type == 'image':
+                anchor_vals = ['tl', 'tr', 'bl', 'br', 'ce']
+                if obj.anchor not in anchor_vals:
+                    self.log.error('anchor must be ' + ' '.join(anchor_vals))
+
+                source = Image.open(obj.source)
                 source = source.convert('RGBA')
 
-                _op = int(effect['opacity'] * 255)
+                _op = int(obj.opacity * 255)
 
                 source = ImageChops.multiply(source,
                     Image.new('RGBA', source.size, (255, 255, 255, _op))
                 )
-                effect['source'] = source
+                obj.source = source
 
-            if('anchor' in effect):
-                anchor_vals = ['tl', 'tr', 'bl', 'br', 'ce']
-                if(effect['anchor'] not in anchor_vals):
-                    self.log.error('anchor must be ' + ' '.join(anchor_vals))
-
-            self.all.append(effect)
+            self.all.append(obj)
 
 
     def apply(self, index, frame, pix_fmt):
         img = frame.to_image().convert('RGBA')
 
         for item in self.sheet[index]:
-            pars = self.all[item]
+            obj = self.all[item]
 
             obj_img = Image.new('RGBA', img.size, (255, 255, 255, 0))
             draw = ImageDraw.Draw(obj_img)
 
-            if(pars['type'] == 'text'):
+            if obj._type == 'text':
+                text_w, text_h = draw.textsize(obj.content, font=obj.font)
+                pos = _apply_anchor(obj.x, obj.y, text_w, text_h, 'ce')
+                draw.text(pos, obj.content, font=obj.font, fill=obj.fill)
 
-                tw, th = draw.textsize(pars['content'], font=pars['font'])
+            if obj._type == 'rectangle':
+                draw.rectangle([obj.x1, obj.y1, obj.x2, obj.y2],
+                    fill=obj.fill, width=obj.width, outline=obj.outline)
 
-                new_x, new_y = _apply_anchor(pars['x'], pars['y'], tw, th, 'ce')
+            if obj._type == 'ellipse':
+                draw.ellipse([obj.x1, obj.y1, obj.x2, obj.y2],
+                    fill=obj.fill, width=obj.width, outline=obj.outline)
 
-                draw.text((new_x, new_y), pars['content'], font=pars['font'],
-                    fill=pars['fill'])
-
-            if(pars['type'] == 'rectangle'):
-                draw.rectangle([pars['x1'], pars['y1'], pars['x2'], pars['y2']],
-                    fill=pars['fill'], width=pars['width'], outline=pars['outline'])
-
-            if(pars['type'] == 'ellipse'):
-                draw.ellipse([pars['x1'], pars['y1'], pars['x2'], pars['y2']],
-                    fill=pars['fill'], width=pars['width'], outline=pars['outline'])
-
-            if(pars['type'] == 'image'):
-                img_w, img_h = pars['source'].size
-                pos = _apply_anchor(pars['x'], pars['y'], img_w, img_h, pars['anchor'])
-                obj_img.paste(pars['source'], pos)
+            if obj._type == 'image':
+                img_w, img_h = obj.source.size
+                pos = _apply_anchor(obj.x, obj.y, img_w, img_h, obj.anchor)
+                obj_img.paste(obj.source, pos)
 
             img = Image.alpha_composite(img, obj_img)
 
--- auto_editor/vanparse.py
+++ auto_editor/vanparse.py
@@ -1,5 +1,4 @@
 import sys
-import difflib
 
 class ParserError(Exception):
     pass
@@ -35,32 +34,37 @@ def out(text):
     print('\n'.join(wrapped_lines))
 
 def print_option_help(args, option):
+    from dataclasses import fields, _MISSING_TYPE
+
     text = '  ' + ', '.join(option['names']) + '\n    ' + option['help'] + '\n\n'
-    if(option['keywords'] != []):
+    if option['dataclass'] is not None:
         text += '    Arguments:\n    '
-        for n, item in enumerate(option['keywords']):
-            [[k, v]] = item.items()
 
-            if(k != 'type'):
-                _, default = v
-                text += '{' + k
-                text += '' if default == '' else '=' + str(default)
-                text += '}' if n + 1 == len(option['keywords']) else '},'
+        args = []
+        for field in fields(option['dataclass']):
+            if field.name != '_type':
+                arg = '{' + field.name
+                if not isinstance(field.default, _MISSING_TYPE):
+                    arg += '=' + str(field.default)
+                arg += '}'
+                args.append(arg)
 
-    if(option['manual'] != ''):
+        text += ','.join(args)
+
+    if option['manual'] != '':
         text += '{}\n\n'.format(indent(option['manual'], '    '))
 
-    if(option['keywords'] != []):
+    if option['dataclass'] is not None:
         pass
-    elif(option['action'] == 'default'):
+    elif option['action'] == 'default':
         text += '    type: ' + option['type'].__name__
         text += '\n    default: {}\n'.format(option['default'])
-        if(option['range'] is not None):
+        if option['range'] is not None:
             text += '    range: ' +  option['range'] + '\n'
 
-        if(option['choices'] is not None):
+        if option['choices'] is not None:
             text += '    choices: ' +  ', '.join(option['choices']) + '\n'
-    elif(option['action'] in ['store_true', 'store_false']):
+    elif option['action'] in ['store_true', 'store_false'] :
         text += '    type: flag\n'
     else:
         text += '    type: unknown\n'
@@ -83,9 +87,9 @@ def print_program_help(the_args):
 def get_option(item, the_args):
     for options in the_args:
         for option in options:
-            if(option['action'] != 'text' and option['action'] != 'blank'):
+            if option['action'] != 'text' and option['action'] != 'blank':
                 dash = list(map(lambda n: n.replace('_', '-'), option['names']))
-                if((item in option['names'] or item in dash)):
+                if item in option['names'] or item in dash:
                     return option
     return None
 
@@ -93,7 +97,7 @@ def _to_key(val):
     # (val: dict) -> str
     return val['names'][0].replace('-', '')
 
-class ArgumentParser():
+class ArgumentParser:
     def __init__(self, program_name, version, description=None):
         self.program_name = program_name
         self._version = version
@@ -109,7 +113,7 @@ class ArgumentParser():
             'choices': None,
             'group': None,
             'help': '',
-            'keywords': [],
+            'dataclass': None,
             'hidden': False,
             'manual': '',
         }
@@ -123,7 +127,7 @@ class ArgumentParser():
             my_dict[key] = item
 
         for key, item in kwargs.items():
-            if(key not in self.kwarg_defaults):
+            if key not in self.kwarg_defaults:
                 raise ValueError('key {} not found.'.format(key))
             my_dict[key] = item
 
@@ -139,84 +143,89 @@ class ArgumentParser():
         self.args.append({'action': 'blank'});
 
     def parse_args(self, sys_args):
-        if(sys_args == [] and self.description):
+        if sys_args == [] and self.description:
             out(self.description)
             sys.exit()
 
-        if(sys_args == ['-v'] or sys_args == ['-V']):
+        if sys_args == ['-v'] or sys_args == ['-V']:
             out('{} version {}'.format(self.program_name, self._version))
             sys.exit()
 
         return ParseOptions(sys_args, self.args)
 
 
-"""
-    Positional Arguments
-        --rectangle 0,end,10,20,20,30,#000, ...
-
-    Keyword Arguments
-        --rectangle start=0,end=end,x1=10, ...
-
-"""
-
-class ParseOptions():
+class ParseOptions:
 
     @staticmethod
-    def parse_parameters(unsplit_arguments, op):
+    def parse_dataclass(unsplit_arguments, op):
+        """
+        Positional Arguments
+            --rectangle 0,end,10,20,20,30,#000, ...
+
+        Keyword Arguments
+            --rectangle start=0,end=end,x1=10, ...
+        """
+
+        from dataclasses import fields
 
         ARG_SEP = ','
         KEYWORD_SEP = '='
-        REQUIRED = ''
 
-        dic = {}
+        d_name = op['dataclass'].__name__
+
         keys = []
-        for key in op['keywords']:
-            [[k, v]] = key.items()
-            keys.append(k)
-            dic[k] = v
+        for field in fields(op['dataclass']):
+            keys.append(field.name)
+
+        kwargs = {}
+        args = []
 
         allow_positional_args = True
 
         for i, item in enumerate(unsplit_arguments.split(ARG_SEP)):
-            if(i+1 > len(keys)):
-                raise ParserError(f"Too many arguments, starting with '{item}'.")
+            if i+1 > len(keys):
+                raise ParserError(f"{d_name} has too many arguments, starting "
+                    f"with '{item}'.")
 
-            if(KEYWORD_SEP in item):
+            if KEYWORD_SEP in item:
                 allow_positional_args = False
 
                 parameters = item.split(KEYWORD_SEP)
-                if(len(parameters) > 2):
-                    raise ParserError(f"Invalid Syntax: '{item}'.")
+                if len(parameters) > 2:
+                    raise ParserError(f"{d_name} invalid syntax: '{item}'.")
                 key, val = parameters
-                if(key not in dic):
-                    raise ParserError(f"Parameter '{key}' does not exist.")
-                dic[key] = (dic[key][0], val)
-            elif(allow_positional_args):
-                dic[keys[i]] = (dic[keys[i]][0], item)
-            else:
-                raise ParserError('Positional argument follows keyword argument.')
+                if key not in keys:
+                    raise ParserError(f"{d_name} got an unexpected keyword '{key}'")
 
-        # Check if any required args are not specified.
-        for key, type_val in dic.items():
-            if(type_val[1] == REQUIRED):
-                raise ParserError(f"Parameter '{key}' is required.")
-        return dic
+                kwargs[key] = val
+            elif allow_positional_args:
+                args.append(item)
+            else:
+                raise ParserError(f'{d_name} positional argument follows keyword argument.')
+
+        try:
+            dataclass_instance = op['dataclass'](*args, **kwargs)
+        except TypeError as err:
+            err_list = [d_name] + str(err).split(' ')[1:]
+            raise ParserError(' '.join(err_list))
+
+        return dataclass_instance
 
     def __init__(self, sys_args, *args):
         # Set the default options.
         for options in args:
             for option in options:
-                if(option['action'] == 'text' or option['action'] == 'blank'):
+                if option['action'] == 'text' or option['action'] == 'blank':
                     continue
 
                 key = _to_key(option)
-                if(option['action'] == 'store_true'):
+                if option['action'] == 'store_true':
                     value = False
-                elif(option['action'] == 'store_false'):
+                elif option['action'] == 'store_false':
                     value = True
-                elif(option['nargs'] != 1):
+                elif option['nargs'] != 1:
                     value = []
-                elif(option['default'] is None):
+                elif option['default'] is None:
                     value = None
                 else:
                     value = option['type'](option['default'])
@@ -237,45 +246,51 @@ class ParseOptions():
             option = get_option(item, the_args=args)
 
             def error_message(args, item, label):
+                import difflib
 
                 def all_names(args):
                     name_set = set()
                     for options in args:
                         for opt in options:
-                            if(opt['action'] not in ['text', 'blank']):
+                            if opt['action'] not in ['text', 'blank']:
                                 for names in opt['names']:
                                     name_set.add(names)
                     return name_set
 
                 opt_list = all_names(args)
+
                 close_matches = difflib.get_close_matches(item, opt_list)
-                if(close_matches):
+                if close_matches:
                     return 'Unknown {}: {}\n\n    Did you mean:\n        '.format(
                         label, item) + ', '.join(close_matches)
                 return 'Unknown {}: {}'.format(label, item)
 
-            if(option is None):
+            if option is None:
                 # Unknown Option!
                 if(setting_inputs and (option_list != 'input' or (option_list == 'input' and not item.startswith('-')))):
                     # Option is actually an input file, like example.mp4
 
-                    if(option_list != 'input'):
-                        _op = used_options[-1]
-                        if(_op['keywords'] != []):
-                            item = self.parse_parameters(item, _op)
+                    if used_options and used_options[-1]['dataclass'] is not None:
+                        # Parse comma args instead.
+                        list_type = None
+                        item = self.parse_dataclass(item, used_options[-1])
+
                     my_list.append(item)
                 else:
                     raise ParserError(error_message(args, item, label))
             else:
                 # We found the option.
-                if(option_list is not None):
-                    setattr(self, option_list, list(map(list_type, my_list)))
+                if option_list is not None:
+                    if list_type is not None:
+                        setattr(self, option_list, list(map(list_type, my_list)))
+                    else:
+                        setattr(self, option_list, my_list)
 
                 setting_inputs = False
                 option_list = None
                 my_list = []
 
-                if(option in used_options):
+                if option in used_options:
                     raise ParserError('Cannot repeat option {} twice.'.format(option['names'][0]))
 
                 used_options.append(option)
@@ -283,37 +298,41 @@ class ParseOptions():
                 key = _to_key(option)
                 _set.append(key)
 
-                nextItem = None if i == len(sys_args) - 1 else sys_args[i+1]
-                if(nextItem == '-h' or nextItem == '--help'):
+                next_arg = None if i == len(sys_args) - 1 else sys_args[i+1]
+                if next_arg == '-h' or next_arg == '--help':
                     print_option_help(args, option)
                     sys.exit()
 
-                if(option['nargs'] != 1):
+                if option['nargs'] != 1:
                     setting_inputs = True
                     option_list = key
                     list_type = option['type']
-                elif(option['action'] == 'store_true'):
+                elif option['action'] == 'store_true':
                     value = True
-                elif(option['action'] == 'store_false'):
+                elif option['action'] == 'store_false':
                     value = False
                 else:
-                    if(nextItem is None and option['nargs'] == 1):
+                    if next_arg is None and option['nargs'] == 1:
                         raise ParserError(f"{option['names'][0]} needs argument.")
-                    value = option['type'](nextItem)
 
-                    if(option['choices'] is not None and value not in option['choices']):
+                    value = option['type'](next_arg)
+
+                    if option['choices'] is not None and value not in option['choices']:
                         option_name = option['names'][0]
                         my_choices = ', '.join(option['choices'])
 
-                        raise ParserError('{} is not a choice for {}\nchoices are:\n  {}'.format(
-                            value, option_name, my_choices))
+                        raise ParserError(f'{value} is not a choice for {option_name}\n'
+                            f'choices are:\n  {my_choices}')
                     i += 1
                 setattr(self, key, value)
 
             i += 1
-        if(setting_inputs):
-            setattr(self, option_list, list(map(list_type, my_list)))
+        if setting_inputs:
+            if list_type is not None:
+                setattr(self, option_list, list(map(list_type, my_list)))
+            else:
+                setattr(self, option_list, my_list)
         setattr(self, '_set', _set)
-        if(self.help):
+        if self.help:
             print_program_help(args)
             sys.exit()
