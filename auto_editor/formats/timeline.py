ff5fd72116b99a84c32e1efb6e2a501695d36bb8
WyattBlue <wyattblue@auto-editor.com>
2022-03-14 21:23:22 -0400

    Harden timeline API

--- auto_editor/formats/timeline.py
+++ auto_editor/formats/timeline.py
@@ -8,27 +8,56 @@ import json
 
 from auto_editor.utils.log import Log
 
+def check_attrs(data, log: Log, *attrs: str):
+    for attr in attrs:
+        if attr not in data:
+            log.error(f"'{attr}' attribute not found!")
+
+def check_file(path: str, log: Log):
+    if not os.path.isfile(path):
+        log.error(f"Could not locate media file: '{path}'")
+
 def read_json_timeline(json_file: str, log: Log):
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    source = data['source']
-    if not os.path.isfile(source):
-        log.error(f"Could not locate media file: '{source}'")
+    check_attrs(data, log, 'version')
 
-    background = data['background']
+    if data['version'] not in ('0.1.0', '0.2.0'):
+        log.error(f'Unsupported version: {version}')
 
-    return background, source, data['chunks']
+    if data['version'] == '0.1.0':
+        check_attrs(data, log, 'source', 'chunks')
+        check_file(data['source'], log)
+
+        return '#000', data['source'], data['chunks']
+
+    # version 0.2.0
+    check_attrs(data, log, 'source', 'background', 'chunks', 'timeline')
+    check_file(data['source'], log)
+
+    return data['background'], data['source'], data['chunks']
 
 
-def make_json_timeline(media_file, out, obj_sheet, chunks, fps, background, log):
-    data = {
-        'version': '0.2.0',
-        'source': os.path.abspath(media_file),
-        'background': background,
-        'chunks': chunks,
-        'timeline': obj_sheet.all,
-    }
+def make_json_timeline(version, media_file, out, obj_sheet, chunks, fps, background, log):
+
+    if version not in ('0.1.0', '0.2.0'):
+        log.error(f'Version {version} is not supported!')
+
+    if version == '0.1.0':
+        data = {
+            'version': '0.1.0',
+            'source': os.path.abspath(media_file)
+            'chunks': chunks,
+        }
+    if version == '0.2.0':
+        data = {
+            'version': '0.2.0',
+            'source': os.path.abspath(media_file),
+            'background': background,
+            'chunks': chunks,
+            'timeline': obj_sheet.all,
+        }
 
     if isinstance(out, str):
         if not out.endswith('.json'):
@@ -39,4 +68,3 @@ def make_json_timeline(media_file, out, obj_sheet, chunks, fps, background, log)
     else:
         json.dump(data, sys.stdout, indent=2, default=lambda o: o.__dict__)
         print('')  # Flush stdout
-
