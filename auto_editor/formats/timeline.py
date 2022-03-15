2d699687f01b68585a0426c6b10687e614774e46
WyattBlue <wyattblue@auto-editor.com>
2022-03-15 15:49:13 -0400

    Add chunk validation

--- auto_editor/formats/timeline.py
+++ auto_editor/formats/timeline.py
@@ -17,6 +17,40 @@ def check_file(path: str, log: Log):
     if not os.path.isfile(path):
         log.error(f"Could not locate media file: '{path}'")
 
+
+def validate_chunks(chunks, log: Log):
+
+    if len(chunks) == 0:
+        log.error('Chunks are empty!')
+
+    new_chunks = []
+
+    prev_end = None
+
+    for i, chunk in enumerate(chunks):
+
+        if len(chunk) != 3:
+            log.error('Chunk has incorrect length.')
+
+        if i == 0 and chunk[0] != 0:
+            log.error('First chunk must start with 0')
+
+        if chunk[1] - chunk[0] < 1:
+            log.error('Chunk duration must be at least 1')
+
+        if chunk[2] <= 0 or chunk[2] > 99999:
+            log.error('Chunk speed range must be >0 and <=99999')
+
+        if prev_end is not None and chunk[0] != prev_end:
+            log.error(f'Chunk disjointed at {chunk}')
+
+        prev_end = chunk[1]
+
+        new_chunks.append((chunk[0], chunk[1], float(chunk[2])))
+
+    return new_chunks
+
+
 def read_json_timeline(json_file: str, log: Log):
     with open(json_file, 'r') as f:
         data = json.load(f)
@@ -30,13 +64,17 @@ def read_json_timeline(json_file: str, log: Log):
         check_attrs(data, log, 'source', 'chunks')
         check_file(data['source'], log)
 
+        chunks = validate_chunks(data['chunks'], log)
+
         return '#000', data['source'], data['chunks']
 
     # version 0.2.0
     check_attrs(data, log, 'source', 'background', 'chunks', 'timeline')
     check_file(data['source'], log)
 
-    return data['background'], data['source'], data['chunks']
+    chunks = validate_chunks(data['chunks'], log)
+
+    return data['background'], data['source'], chunks
 
 
 def make_json_timeline(version, media_file, out, obj_sheet, chunks, fps, background, log):
