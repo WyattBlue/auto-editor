97b2dc3acbfc4c44b6751e95dd5704ab26262943
WyattBlue <wyattblue@auto-editor.com>
2022-09-02 13:25:12 -0400

    Add M1 FFmpeg and FFprobe

--- ae-ffmpeg/ae_ffmpeg/__init__.py
+++ ae-ffmpeg/ae_ffmpeg/__init__.py
@@ -1,10 +1,15 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 import os.path
-from platform import system
+from platform import system, machine
 
 def get_path() -> str:
-    program = "ffmpeg.exe" if system() == "Windows" else "ffmpeg"
+    _os = system()
+    _arch = machine().lower()
+    _interdir = _os if _os != "Darwin" else f"{_os}-{_arch}"
+    program = "ffmpeg.exe" if _os == "Windows" else "ffmpeg"
+
     dirpath = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(dirpath, system(), program)
+    file_path = os.path.join(dirpath, _interdir, program)
+
     return file_path if os.path.isfile(file_path) else "ffmpeg"
