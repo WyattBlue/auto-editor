2a2825dab8e3ebd6db3485e9f81eb732c74abfcd
WyattBlue <wyattblue@auto-editor.com>
2023-10-19 20:45:16 -0400

    Upgrade deps

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ setup(
     zip_safe=True,
     install_requires=[
         "numpy>=1.22.0",
-        "pillow==10.0.1",
-        "pyav==11.1.0",
+        "pillow==10.1.0",
+        "pyav==11.2.0",
         "ae-ffmpeg==1.1.*",
     ],
     python_requires=">=3.10",
