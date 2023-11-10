422db98b38e9f833ce770f8b8185006b85dd524b
WyattBlue <wyattblue@auto-editor.com>
2023-11-10 01:36:09 -0500

    Use pyav for getting media info

--- auto_editor/subcommands/test.py
+++ auto_editor/subcommands/test.py
@@ -562,7 +562,7 @@ def main(sys_args: list[str] | None = None):
     #  Issue 280
     def SAR():
         out = run.main(["resources/SAR-2by3.mp4"], [])
-        assert checker.check(out).videos[0].sar == "2:3"
+        assert checker.check(out).videos[0].sar == Fraction(2, 3)
 
         return out
 
