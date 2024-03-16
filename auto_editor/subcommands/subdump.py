8127d19f61832c0699c0b42b990302dfb955a827
WyattBlue <wyattblue@auto-editor.com>
2024-03-16 01:11:12 -0400

    Improve subdump

--- auto_editor/subcommands/subdump.py
+++ auto_editor/subcommands/subdump.py
@@ -1,25 +1,22 @@
 import sys
-from dataclasses import dataclass, field
-
-from auto_editor.utils.log import Log
 
 import av
+from av.subtitles.subtitle import SubtitleSet
 
 
 def main(sys_args: list[str] = sys.argv[1:]) -> None:
     for i, input_file in enumerate(sys_args):
         with av.open(input_file) as container:
             for s in range(len(container.streams.subtitles)):
-                print(f"file: {input_file} ({s})")
+                print(f"file: {input_file} ({s}:{container.streams.subtitles[s].name})")
                 for packet in container.demux(subtitles=s):
                     for item in packet.decode():
-                        if type(item) is av.subtitles.subtitle.SubtitleSet and item:
+                        if type(item) is SubtitleSet and item:
                             if item[0].type == b"ass":
                                 print(item[0].ass.decode("utf-8"))
-                            else:
+                            elif item[0].type == b"text":
                                 print(item[0].text)
         print("------")
-
 
 
 if __name__ == "__main__":
