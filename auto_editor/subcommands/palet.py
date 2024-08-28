7de26aae4c530dd56eb936eaf3b5300c8fd2c420
WyattBlue <wyattblue@auto-editor.com>
2024-08-28 02:47:40 -0400

    Put standard env in function

--- auto_editor/subcommands/palet.py
+++ auto_editor/subcommands/palet.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import sys
 
 from auto_editor.lang.palet import Lexer, Parser, env, interpret
+from auto_editor.lang.stdenv import make_standard_env
 from auto_editor.lib.err import MyError
 
 
@@ -11,6 +12,7 @@ def main(sys_args: list[str] = sys.argv[1:]) -> None:
         with open(sys_args[0], encoding="utf-8", errors="ignore") as file:
             program_text = file.read()
 
+        env.update(make_standard_env())
         try:
             interpret(env, Parser(Lexer(sys_args[0], program_text, True)))
         except (MyError, ZeroDivisionError) as e:
