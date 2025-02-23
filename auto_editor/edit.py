05a03caec460745f78458e792f1cb782586af3a7
WyattBlue <wyattblue@auto-editor.com>
2025-02-22 21:30:50 -0500

    Progress bar, use canonical audio name

--- auto_editor/edit.py
+++ auto_editor/edit.py
@@ -376,7 +376,7 @@ def edit_media(paths: list[str], args: Args, log: Log) -> None:
             name = output_stream.codec.canonical_name
             encoder_titles.append(name if no_color else f"\033[95m{name}")
         if audio_streams:
-            name = audio_streams[0].name
+            name = audio_streams[0].name.canonical_name  # type: ignore
             encoder_titles.append(name if no_color else f"\033[96m{name}")
         if subtitle_streams:
             name = subtitle_streams[0].name
