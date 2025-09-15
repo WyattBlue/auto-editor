4085fdc30a5c437b9de561540f85acbcddad417b
WyattBlue <wyattblue@auto-editor.com>
2025-09-14 20:47:27 -0400

    Add nim.cfg

--- src/ffmpeg.nim
+++ src/ffmpeg.nim
@@ -1,27 +1,3 @@
-{.passC: "-I./build/include".}
-when defined(macosx):
-  {.passL: "-framework VideoToolbox -framework AudioToolbox -framework CoreFoundation -framework CoreMedia -framework CoreVideo".}
-when defined(linux):
-  {.passL: "-L./build/lib/x86_64-linux-gnu -L./build/lib64".}
-{.passL: "-L./build/lib -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil".}
-{.passL: "-lmp3lame -lopus -lvpx -lx264 -ldav1d -lSvtAv1Enc".}
-
-when defined(enable_whisper):
-  {.passL: "-lwhisper -lggml -lggml-cpu -lggml-blas -lggml-metal -lggml-base".}
-  when defined(macosx):
-    {.passL: "-framework Accelerate -framework Metal -framework MetalKit -framework Foundation".}
-
-when defined(enable_hevc):
-  {.passL: "-lx265".}
-
-when defined(enable_hevc) or defined(enable_whisper):
-  when defined(macosx): # C++ linkers
-    {.passL: "-lc++".}
-  else:
-    {.passL: "-lstdc++".}
-
-{.passL: "-lm".}
-
 import std/posix
 
 type AVRational* {.importc, header: "<libavutil/rational.h>", bycopy.} = object
