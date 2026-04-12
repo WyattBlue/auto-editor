71426f95e500b6f7be95a1118932388feda44d5f
WyattBlue <wyattblue@auto-editor.com>
2026-04-12 14:43:36 -0400

    Make desc.main not raise

--- src/cmds/desc.nim
+++ src/cmds/desc.nim
@@ -1,15 +1,22 @@
 import ../[av, ffmpeg]
 
-proc main*(args: seq[string]) =
+proc main*(args: seq[string]) {.raises: [].} =
   av_log_set_level(AV_LOG_QUIET)
 
+  var formatContext: ptr AVFormatContext
   for inputFile in args:
-    let formatContext = av.openFormatCtx(inputFile.cstring)
-    var entry = av_dict_get(formatContext.metadata, "description", nil, 0)
+    try:
+      formatContext = av.openFormatCtx(inputFile.cstring)
+    except IOError:
+      continue
 
-    if entry != nil:
-      stdout.write("\n" & $entry.value & "\n\n")
-    else:
-      stdout.write("\nNo description.\n\n")
+    var entry = av_dict_get(formatContext.metadata, "description", nil, 0)
+    try:
+      if entry != nil:
+        stdout.write("\n" & $entry.value & "\n\n")
+      else:
+        stdout.write("\nNo description.\n\n")
+    except IOError:
+      discard
 
     avformat_close_input(addr formatContext)
